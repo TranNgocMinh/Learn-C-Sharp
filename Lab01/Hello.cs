b83907f0f7702091c337f350dbc23de544e791a1
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-01-17 14:26:00 +0700

    Create Hello.cs

---
+++ Lab01/Hello.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadKey();
+        }
+    }
+}
