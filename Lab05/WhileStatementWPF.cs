9eb104919337bd3ba8f6ee4c2840a8b93ab12295
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-01-17 15:45:19 +0700

    Create WhileStatementWPF.cs

---
+++ Lab05/WhileStatementWPF.cs
@@ -0,0 +1,66 @@
+// download WhileStatement.exe
+// download Minh.txt file
+// run WhileStatement.exe
+// click Open File button and select Minh.txt from hard disk
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WhileStatement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private OpenFileDialog openFileDialog = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.FileOk += openFileDialogFileOk;
+        }
+
+        private void openFileClick(object sender, RoutedEventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void openFileDialogFileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string fullPathname = openFileDialog.FileName;
+            FileInfo src = new FileInfo(fullPathname);
+            fileName.Text = src.FullName;
+            TextReader reader = src.OpenText();
+            displayData(reader);
+        }
+
+        private void displayData(TextReader reader)
+        {
+            source.Text = "";
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                source.Text += line + '\n';
+                line = reader.ReadLine();
+            }
+
+            reader.Close();
+        }
+    }
+}
