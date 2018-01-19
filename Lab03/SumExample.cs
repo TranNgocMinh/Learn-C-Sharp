b18e08a2ede51a7841b0f1d5263f9908eaa532ad
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-01-19 16:28:04 +0700

    Create SumExample.cs

---
+++ Lab03/SumExample.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        void Sum(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+        int Sum(int x, int y){
+          return (x+y);
+        }
+     
+    }
+}
