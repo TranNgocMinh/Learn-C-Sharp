64be86fb1716655d54d15649c5b2e7bf3dcc1594
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-01-25 14:07:10 +0700

    Update Test.cs

--- Lab04/Test.cs
+++ Lab04/Test.cs
@@ -1,54 +1,52 @@
 using System;
 
-namespace Example
+namespace Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // nhập chiều dài và rộng của hình chữ nhật
-            double CD = 4.5;
-            double CR = 3.0;
-            // tính chu vi hình chữ nhật
-            double CV = (CD+CR)*2;
-            // tính diện tích hình chữ nhật
-            double DT = CD*CR;
-            // hiển thị chu vi
-            System.Console.WriteLine(CV.toString());
-            // hiển thị diện tích
-            System.Console.WriteLine(DT.toString());
-            // Hiển thị chu vi bằng phương thức Chu_vi_1
-            ...
-            // Hiển thị diện tích bằng phương thức Dien_tich_1
-            ...
-            // Hiển thị chu vi bằng phương thức Chu_vi_2
-            ...
-            // Hiển thị diện tích bằng phương thức Dien_tich_2
-            ...  
+           //*****Cách sử dụng phương thức Chan_Le***************
+            Console.WriteLine("Vui long nhap mot so nguyen:");
+            int number = int.Parse(Console.ReadLine());
+            if (Chan_Le(number))
+                Console.WriteLine("Toi la so chan!");
+            else
+                Console.WriteLine("Toi la so le!");
+            //****************************************************
+            //**Sử dụng phương thức Phep_Tinh**********************
+             Console.WriteLine("Vui long nhap so thu nhat:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vui long nhap so thu hai:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vui long nhap phep tinh +, -, *, hay /");
+            char pt = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Phep_Tinh(a, b, pt);
+            //*******************************************************
+            Console.ReadKey();
         }
         // Yêu cầu:
-        // 1. Viết phương thức có kiểu trả về:
-        //   a. Phương thức Chu_vi gồm hai tham số tính chu vi hình chữ nhật
-        Chu_vi_1 () 
-        {
-            ...
+        //1. Viết phương thức Chan_Le để kiểm tra một số nguyên là số chẵn hay lẻ.
+        // Nếu là chẵn thì hiển thị "Toi la so chan!", nếu là lẻ thì hiển thị
+        // "Toi la so le!".
+        public static bool Chan_Le(int number) {
+            ....
         }
-        //   b. Phương thức Dien_tich gồm hai tham số tính diện tích hình chữ nhật
-        Dien_tich_1()
-        {
-            ...
+        // 2. Viết phương thức Phep_Tinh để thực hiện +, -, *, / hai số thực
+        // được nhập từ bàn phím và phép tính cũng được nhập từ bàn phím.
+         public static void Phep_Tinh(double a, double b, char pt) {
+            switch (pt) { 
+                case '+':
+                    Console.WriteLine(a.ToString()+"+"+b.ToString()+"="+(a+b).ToString());
+                    break;
+               ........
+                default:
+                    Console.WriteLine("Vui long nhap phep tinh + - * /");
+                    break;
+            
+            }
+        
         }
-        // 2. Viết phương thức không có kiểu trả về:
-        //   a. Phương thức Chu_vi gồm hai tham số tính chu vi hình chữ nhật
-        Chu_vi_2() 
-        {
-            ...
-        }
-        //   b. Phương thức Dien_tich gồm hai tham số tính diện tích hình chữ nhật
-        Dien_tich_2()
-        {
-            ...
-        }
-     
     }
 }
