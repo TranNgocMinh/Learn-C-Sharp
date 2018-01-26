71b7c27c2a70931e2cc1162b93dc9eb846cc5b65
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-01-26 09:55:30 +0700

    Create Test.cs

---
+++ Lab05/Test.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Yêu cầu: sử dụng các vòng lặp for, while và do để
+           // viết chương trình tính tổng các số chẵn từ 1 đến n, với
+           //n là số nguyên được nhập từ bàn phím.
+            //*******************************************************
+            Console.ReadKey();
+        }
+    }
+}
