fd05b011d327b481f92c8e784c2db6fe25fff150
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-02-21 12:31:28 +0700

    Create Hashtable.cs

---
+++ Lab12/Hashtable.cs
@@ -0,0 +1,34 @@
+//**********COLLECTION - HASHTABLE************************
+// The Hashtable class represents a collection of key-and-value pairs that are organized based on the hash code of the key.
+using System;
+using System.Collections;
+
+namespace CollectionsApplication {
+   class Program {
+      static void Main(string[] args) {
+         Hashtable ht = new Hashtable();
+         
+         ht.Add("001", "Zara Ali");
+         ht.Add("002", "Abida Rehman");
+         ht.Add("003", "Joe Holzner");
+         ht.Add("004", "Mausam Benazir Nur");
+         ht.Add("005", "M. Amlan");
+         ht.Add("006", "M. Arif");
+         ht.Add("007", "Ritesh Saikia");
+         
+         if (ht.ContainsValue("Nuha Ali")) {
+            Console.WriteLine("This student name is already in the list");
+         } else {
+            ht.Add("008", "Nuha Ali");
+         }
+         
+         // Get a collection of the keys.
+         ICollection key = ht.Keys;
+         
+         foreach (string k in key) {
+            Console.WriteLine(k + ": " + ht[k]);
+         }
+         Console.ReadKey();
+      }
+   }
+}
