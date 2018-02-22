9aff556f3c92068765217a875d23b0fb589949fe
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-02-22 13:45:43 +0700

    Create Main.cs

---
+++ Lab16/Main.cs
@@ -0,0 +1,29 @@
+ static void Main(string[] args)
+        {
+
+            LoadData();
+           /* Khoa kh = new Khoa();
+            kh.MaKhoa = "KH03";
+            kh.TenKhoa = "Moi Truong";
+            if(InsertData(kh) > 0)
+                Console.WriteLine("Them du lieu thanh cong!"); 
+            else
+                Console.WriteLine("Them du lieu that bai!");*/
+        /*  Khoa kh = new Khoa();
+          kh.MaKhoa = "KH03";
+          kh.TenKhoa = "Cong nghe sinh hoc";
+          if(UpdateData(kh) > 0)
+              Console.WriteLine("Cap nhat du lieu thanh cong!"); 
+          else
+              Console.WriteLine("Cap nhat du lieu that bai!");*/
+         /*   Khoa kh = new Khoa();
+            kh.MaKhoa = "KH03";
+           
+            if (DeleteData(kh) > 0)
+                Console.WriteLine("Xoa du lieu thanh cong!");
+            else
+                Console.WriteLine("Xoa du lieu that bai!");
+            */
+            Console.WriteLine("Nhan phim bat ky de dong ung dung."); 
+            Console.ReadKey(); 
+        }
