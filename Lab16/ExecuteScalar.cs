727a4e46ca94fba62ef0ee6b3bed0c2794a8bfa1
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-02-22 12:00:52 +0700

    Create ExecuteScalar.cs

---
+++ Lab16/ExecuteScalar.cs
@@ -0,0 +1,18 @@
+ public string GetPersonName(int MaSV) 
+{ 
+    string connectionString = ConfigurationManager.ConnectionStrings["QLSV"]?.ConnectionString; 
+    using (SqlConnection connection = new SqlConnection(connectionString)) 
+    using (SqlCommand command = new SqlCommand( 
+        "SELECT TenSV FROM SinhVien WHERE MaSV = @MaSV", connection)) 
+    { 
+        command.Parameters.Add("MaSV", SqlDbType.Int).Value = MaSV; 
+        connection.Open(); 
+        object result = command.ExecuteScalar(); 
+        string TenSV = null; 
+        if (result != DBNull.Value) 
+        { 
+            TenSV = (string)result; 
+        } 
+        return TenSV; 
+    } 
+} 
