552f5ca261400cbb9ee8408f51afa8d3d05f7c68
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-02-24 14:39:50 +0700

    Create Main.cs

---
+++ Lab17/Main.cs
@@ -0,0 +1,11 @@
+static void Main(string[] args)
+        {
+
+           // InsertData();
+           // UpdateData();
+            DeleteData();
+            LoadData();
+         
+            Console.WriteLine("Nhan phim bat ky de dong ung dung."); 
+            Console.ReadKey(); 
+        }
